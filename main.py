c3711bd2dc68081b7f685131590d26bedfb6a635
xochiquettzal <58644306+xochiquettzal@users.noreply.github.com>
2024-10-06 20:15:28 +0300

    Refactoring.

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from face_detection_app import FaceDetectionApp
 
 if __name__ == "__main__":
     photo_path = "rsrc/photos/person_photo.jpg"
-    video_path = "rsrc/videos/iyiadamin10gunu.mp4"
+    video_path = "rsrc/videos/person_video.mp4"
     app = FaceDetectionApp(photo_path, video_path)
     app.process_video()
